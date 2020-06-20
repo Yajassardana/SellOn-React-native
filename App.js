de3c67cb6ff15c13a48a87c0ff15795e7c095e97
Yajas Sardana <yajassardana@gmail.com>
2020-06-21 04:38:13 +0530

    insert image with uri

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Happy for my first app!</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<Text onPress={() => console.log('pressed')}>
+				Happy for my first app!
+			</Text>
+			<Image
+				source={{
+					uri: 'https://avatars1.githubusercontent.com/u/62782513?s=60&v=4',
+				}}
+			/>
+			{/*Native has onPress instead of on click event*/}
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'white',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
